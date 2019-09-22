89071c140ceb3959501062da903a6ee887b54dfa
FelixZg <46453717+FelixZg@users.noreply.github.com>
2019-09-21 20:25:19 -0400

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿// Modal Elements
+// Main Searchbar
+var mainSearchbar = document.getElementById("main-searchbar");
+// Get modal elements
+var modal = document.getElementById("simpleModal");
+// Get open modal button
+// Login
+var modalLoginBtn = document.getElementById("modalLoginBtn");
+// Register
+var modalRegisterBtn = document.getElementById("modalRegisterBtn");
+// Get close modal button
+var closeBtn = document.getElementById("closeBtn");
+
+// Sign in/up form Elements
+var signUpButton = document.getElementById("signUp");
+var signInButton = document.getElementById("signIn");
+var container = document.getElementById("container");
+
+// Show sign up info
+signUpButton.addEventListener("click", openRegisterModal);
+
+// Show sign in info
+signInButton.addEventListener("click", openLoginModal);
+
+// Listen for click to open modal
+modalLoginBtn.addEventListener("click", openLoginModal);
+modalRegisterBtn.addEventListener("click", openRegisterModal);
+
+// Listen for click close to close modal
+closeBtn.addEventListener("click", closeModal);
+
+// Listen for outside click
+window.addEventListener("click", clickOutside);
+
+// Function to open modal
+function openLoginModal() {
+    closeBtn.style.color = "white";
+    modal.style.display = "block";
+    container.classList.remove("right-panel-active");
+}
+function openRegisterModal() {
+    closeBtn.style.color = "#d81942";
+    modal.style.display = "block";
+    container.classList.add("right-panel-active");
+}
+
+// Function to close modal
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Function to check if clicked outside modal
+function clickOutside(e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
